f27d28a8f00e37a3de114ac59dd73d04b660fd71
john-rock <johnmrock.jr@gmail.com>
2023-11-09 15:46:23 -0500

    small update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-
 export default async function Home() {
-
   const response = await (
     await fetch("http://worldtimeapi.org/api/timezone/America/New_York", {
       next: { tags: ["test"] },
@@ -10,15 +7,20 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen bg-white text-black flex-col items-center justify-between p-24">
-      <p >Minimial reproduction on a fresh Nextjs install confirming revalidateTag() does not work when hosted on Vercel.</p>
+      <p>
+        Minimial reproduction on a fresh Nextjs install confirming
+        revalidateTag() does not work when hosted on Vercel.
+      </p>
       <p className="mt-10">To test:</p>
-      <ol >
-      <li>- Take note of the current time value below.</li>
-      <li>- Hard refresh the page.</li>
-      <li>- Time value should change but does not when hosted on Vercel.</li>
-      <li>- Pull down the repo and run locally, time value will update as expected</li>
+      <ol>
+        <li>- Take note of the current time value below.</li>
+        <li>- Hard refresh the page.</li>
+        <li>- Time value should change but does not when hosted on Vercel.</li>
+        <li>
+          - Pull down the repo and run locally, time value will update as
+          expected
+        </li>
       </ol>
-
 
       <div className="mb-auto mt-auto text-black">{response.datetime}</div>
     </main>
