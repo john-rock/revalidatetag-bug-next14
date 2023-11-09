e591f585225a053297bb49cc91f13322c70f039a
john-rock <johnmrock.jr@gmail.com>
2023-11-09 15:02:46 -0500

    add instructions

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,16 @@ export default async function Home() {
   ).json();
 
   return (
-    <main className="flex min-h-screen bg-white flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen bg-white text-black flex-col items-center justify-between p-24">
+      <p >Minimial reproduction on a fresh Nextjs install confirming revalidateTag() does not work when hosted on Vercel.</p>
+      <p className="mt-10">To test:</p>
+      <ol >
+      <li>- Take note of the current time value below.</li>
+      <li>- Hard refresh the page.</li>
+      <li>- Time value should change but does not when hosted on Vercel.</li>
+      <li>- Pull down the repo and run locally, time value will update as expected</li>
+      </ol>
+
 
       <div className="mb-auto mt-auto text-black">{response.datetime}</div>
     </main>
