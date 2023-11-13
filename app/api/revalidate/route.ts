1beb7bf34981fd37e0700e3fe18112e569d23923
john-rock <johnmrock.jr@gmail.com>
2023-11-13 09:33:55 -0500

    force dynamic on revalidate route

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from 'next/server'
 import { revalidateTag } from 'next/cache'
+
+export const dynamic = "force-dynamic";
  
 export async function GET(request: NextRequest) {
   revalidateTag('test')
